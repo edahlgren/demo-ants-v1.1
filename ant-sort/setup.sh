b0f4b258afb7882a0fdfcd21e9995a17f6c72f41
Erin Dahlgren <eedahlgren@gmail.com>
2019-07-02 15:54:14 -0700

    Cleanup data generation

--- ant-sort/setup.sh
+++ ant-sort/setup.sh
@@ -7,33 +7,38 @@ add-apt-repository ppa:deadsnakes/ppa
 add-apt-repository -y ppa:deadsnakes/ppa
 
 apt-get update
-apt-get install -y make g++ unzip openjdk-8-jdk python3.6
+apt-get install -y curl make g++ unzip openjdk-8-jdk python3.6
+
+# Install python dependencies
+curl https://bootstrap.pypa.io/get-pip.py | python3.6
+pip install pandas scikit-learn
 
 # Unpack the code
 mkdir /root/src && cd /root/src
 unzip /setup/ants.zip
+unzip /setup/cosine.zip
 
-cd /root
-unzip /setup/data.zip
+# Unpack the data
+cd /root && unzip /setup/data.zip
 
-# Create build scripts
+# Generate fake data and normalize real data
+cd /root/data && /root/data/generate.sh
+
+# Create build script
 echo "PWD=\"\$(pwd)\"" >> /root/build.sh
 echo "cd /root/src/ants/source" >> /root/build.sh
 echo "make accl" >> /root/build.sh
 echo "cd \$PWD" >> /root/build.sh
 chmod 755 /root/build.sh
 
-# Create run scripts
-#echo "java -jar /root/src/AntDemo/ant-cluster.jar \"\${@}\"" >> /root/ants1-run.sh
-# chmod 755 /root/ants1-run.sh
-
+# Generate the run script
 echo "/root/src/ants/bin/accl \"\${@}\"" >> /root/run.sh
 chmod 755 /root/run.sh
 
 # Build the code
-/root/build.sh
+cd /root && /root/build.sh
 
 # To run:
 #
-# ~$ ./run.sh -i data/fake_data.csv
+# ~$ ./run.sh -i data/fake_docs.csv
 # ~$ ./run.sh -i data/wine_normal.csv --use-cosine
