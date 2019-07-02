d6a8515147f9c24f23165d402291e2ff168dc88e
Erin Dahlgren <eedahlgren@gmail.com>
2019-07-01 20:28:28 -0700

    Add normalization script and wine data

--- ant-sort/setup.sh
+++ ant-sort/setup.sh
@@ -2,38 +2,38 @@
 
 # Get system dependencies
 apt-get update
-apt-get install -y make g++ unzip openjdk-8-jdk
+apt-get install -y software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+add-apt-repository -y ppa:deadsnakes/ppa
 
-# Copy the code into a src dir
-mkdir /root/src
-cp /setup/AntDemoCLI.zip /root/src/AntDemoCLI.zip
-cp /setup/ants2-CLI.zip /root/src/ants2-CLI.zip
+apt-get update
+apt-get install -y make g++ unzip openjdk-8-jdk python3.6
 
 # Unpack the code
-cd /root/src
-unzip AntDemoCLI.zip
-unzip ants2-CLI.zip
+mkdir /root/src && cd /root/src
+unzip /setup/ants.zip
+
+cd /root
+unzip /setup/data.zip
 
 # Create build scripts
-echo "PWD=\"\$(pwd)\"" >> /root/ants1-build.sh
-echo "cd /root/src/AntDemo" >> /root/ants1-build.sh
-echo "./build.sh" >> /root/ants1-build.sh
-echo "cd \$PWD" >> /root/ants1-build.sh
-chmod 755 /root/ants1-build.sh
-
-echo "PWD=\"\$(pwd)\"" >> /root/ants2-build.sh
-echo "cd /root/src/ants/source" >> /root/ants2-build.sh
-echo "make accl" >> /root/ants2-build.sh
-echo "cd \$PWD" >> /root/ants2-build.sh
-chmod 755 /root/ants2-build.sh
+echo "PWD=\"\$(pwd)\"" >> /root/build.sh
+echo "cd /root/src/ants/source" >> /root/build.sh
+echo "make accl" >> /root/build.sh
+echo "cd \$PWD" >> /root/build.sh
+chmod 755 /root/build.sh
 
 # Create run scripts
-echo "java -jar /root/src/AntDemo/ant-cluster.jar \"\${@}\"" >> /root/ants1-run.sh
-chmod 755 /root/ants1-run.sh
+#echo "java -jar /root/src/AntDemo/ant-cluster.jar \"\${@}\"" >> /root/ants1-run.sh
+# chmod 755 /root/ants1-run.sh
 
-echo "/root/src/ants/bin/accl \"\${@}\"" >> /root/ants2-run.sh
-chmod 755 /root/ants2-run.sh
+echo "/root/src/ants/bin/accl \"\${@}\"" >> /root/run.sh
+chmod 755 /root/run.sh
 
 # Build the code
-/root/ants1-build.sh
-/root/ants2-build.sh
+/root/build.sh
+
+# To run:
+#
+# ~$ ./run.sh -i data/fake_data.csv
+# ~$ ./run.sh -i data/wine_normal.csv --use-cosine
