e8342f131c8b52f5fb7bf4f5ff0b77d0e4c30df6
Erin Dahlgren <eedahlgren@gmail.com>
2019-07-12 20:52:32 -0700

    Change layout of the demo

--- ant-search/shell.sh
+++ ant-search/shell.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-sudo docker run -w /home -it ant-colony-test bash
+mkdir -p shared
+
+# Seems too complicated
+#
+# docker ps -l | grep ant-colony-test | get container ID
+if [ ! -d "shared/src" ]; then
+    docker cp a96177722503:/root/src shared/
+fi
+if [ ! -d "shared/papers" ]; then
+    docker cp a96177722503:/root/papers shared/
+fi
+
+sudo docker run \
+     -w /root \
+     -v $(pwd)/shared:/root/shared \
+     -it \
+     ant-colony-test
